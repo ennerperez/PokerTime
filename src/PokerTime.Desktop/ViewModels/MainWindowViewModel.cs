165cd811ce25f1cd01f3cdeaedb6b583f406009f
Enner Pérez <ennerperez@gmail.com>
2022-07-14 11:21:33 -0500

    Avalonia Approach

---
+++ src/PokerTime.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PokerTime.Desktop.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ src/PokerTime.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PokerTime.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
